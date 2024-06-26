7e4a5252ea0dc85917c1faab9e2508cf6f5fa3a3
Theamitkumarsaini <145679072+Theamitkumarsaini@users.noreply.github.com>
2024-06-26 15:35:53 +0530

    Update __init__.py

--- main/__init__.py
+++ main/__init__.py
@@ -20,12 +20,12 @@ logging.getLogger("telethon").setLevel(logging.WARNING)
 uvloop.install()
 MDB = "mongodb+srv://ggn:ggn@ggn.upuljx5.mongodb.net/?retryWrites=true&w=majority&appName=ggn"
 API_ID ="20544260" #config("API_ID", default=None, cast=int)
-API_HASH ="a0b00461d3fba22aa186fa648d77787e" config("API_HASH", default=None)
-BOT_TOKEN ="6676441705:AAEydZsISO0n5lBYHSCYg5-bsb7jkbH1kXw" config("BOT_TOKEN", default=None)
-SESSION ="AQHDmCIAB8K0u6N-6070IBUeNFWCRDnUV-OKDhJcbOraYNK0YHA3_XyHdrm4NBJMwWoU_EQGL_OEa9rnb5BQt3vBcpX607GjSzp2_H06SEY3_i2GF8gT9bcuMWBd4gGN2zBnofcLESIPiqILO10aWonJnphf2Jr4xegzxIpyGSOKZPuc6kVe5aywYs0t6SNR37vfZ0p9urYRIuDnEzU-JU-iu1l9YaQXTFrJvrtzUKJ3qo1hqUrf0rGeGjmekGx8vlaeKG3s752e29Hhc5sC3Y_BaxYIapnR0CobLMdHPq-wxkp8cfs-HxDnkYLrg97dS77fFK4-BKAvMtaMHfIgx9xmNPGZFgAAAAF81lQuAA" config("SESSION", default=None)
-FORCESUB ="aajajakaak" config("FORCESUB", default=None)
-AUTH ="6389388334" config("AUTH", default=None)
-MONGODB ="mongodb+srv://ggn:ggn@ggn.upuljx5.mongodb.net/?retryWrites=true&w=majority&appName=ggn" config("MONGODB", default=MDB)
+API_HASH ="a0b00461d3fba22aa186fa648d77787e" #config("API_HASH", default=None)
+BOT_TOKEN ="6676441705:AAEydZsISO0n5lBYHSCYg5-bsb7jkbH1kXw" #config("BOT_TOKEN", default=None)
+SESSION ="AQHDmCIAB8K0u6N-6070IBUeNFWCRDnUV-OKDhJcbOraYNK0YHA3_XyHdrm4NBJMwWoU_EQGL_OEa9rnb5BQt3vBcpX607GjSzp2_H06SEY3_i2GF8gT9bcuMWBd4gGN2zBnofcLESIPiqILO10aWonJnphf2Jr4xegzxIpyGSOKZPuc6kVe5aywYs0t6SNR37vfZ0p9urYRIuDnEzU-JU-iu1l9YaQXTFrJvrtzUKJ3qo1hqUrf0rGeGjmekGx8vlaeKG3s752e29Hhc5sC3Y_BaxYIapnR0CobLMdHPq-wxkp8cfs-HxDnkYLrg97dS77fFK4-BKAvMtaMHfIgx9xmNPGZFgAAAAF81lQuAA" #config("SESSION", default=None)
+FORCESUB ="aajajakaak" #config("FORCESUB", default=None)
+AUTH ="6389388334" #config("AUTH", default=None)
+MONGODB ="mongodb+srv://ggn:ggn@ggn.upuljx5.mongodb.net/?retryWrites=true&w=majority&appName=ggn" #config("MONGODB", default=MDB)
 OWN = 6389388334 # edit this
 GROUP = -1001665693055 # edit this
 OWNER_ID = int(config("OWNER_ID", default=OWN))
